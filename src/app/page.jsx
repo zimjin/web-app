88fc99576e9743b8d7e01dcd1841809d188ee409
zimjin <zimjin@tencent.com>
2025-01-03 16:28:52 +0800

    将代码从 react 赚到 next.js

---
+++ src/app/page.jsx
@@ -0,0 +1,26 @@
+import LikeButton from './like-button';
+
+function Header({ title }){
+    return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+export default function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    function handleClick(){
+        setLikes(likes+1);
+    }
+
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship"></Header>
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+
+           <LikeButton /> 
+        </div>
+    );
+}
